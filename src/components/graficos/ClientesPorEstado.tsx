dd270aed2d7d7b0478cfa66d08f65c8617ef2998
William Martinez <williamr1994@gmail.com>
2024-05-18 13:38:43 -0600

    feat: Dashboard UI

---
+++ src/components/graficos/ClientesPorEstado.tsx
@@ -0,0 +1,24 @@
+import { Column } from '@ant-design/charts';
+
+const ClientesPorEstado = () => {
+    const data = [
+        { type: 'Al dia', value: 50, },
+        { type: 'En mora', value: 15, },
+    ];
+
+    const config = {
+        data,
+        xField: 'type',
+        yField: 'value',
+        colorField: 'type',
+        style: {
+            fill: ({ type }) => (type === 'Al dia' ? '#3f8600' : '#cf1322'),
+        }
+    };
+
+    return (
+        <Column {...config} />
+    );
+};
+
+export default ClientesPorEstado;
