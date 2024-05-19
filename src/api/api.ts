b73742f42a0ab03ef23a13eae8bb38923a6f0488
Josue Garcia <josue@sapphiretechglobal.com>
2024-05-18 22:12:26 -0600

    feat: Connecting Dashboard to Backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,152 +1,186 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { API_URL } from '../constants'
-import { axiosBaseQuery } from './axiosBaseQuery'
-import { Aseguradora } from '../types/Aseguradora'
-import { Cliente } from '../types/Cliente'
-import { formatPoliza } from '../utils/utils';
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { API_URL } from "../constants";
+import { axiosBaseQuery } from "./axiosBaseQuery";
+import { Aseguradora } from "../types/Aseguradora";
+import { Cliente } from "../types/Cliente";
+import { formatPoliza } from "../utils/utils";
+import { Dashboard } from "../types/Dashboard";
 
 export const Api = createApi({
-  reducerPath: 'api',
+  reducerPath: "api",
   baseQuery: axiosBaseQuery({
     baseUrl: API_URL,
   }),
-  tagTypes: ['aseguradoras', 'aseguradora', 'clientes', 'cliente', 'polizas', 'poliza'],
+  tagTypes: [
+    "aseguradoras",
+    "aseguradora",
+    "clientes",
+    "cliente",
+    "polizas",
+    "poliza",
+    "dashboard",
+  ],
   endpoints: (builder) => ({
     getCSRFCookie: builder.query<string, void>({
       query: () => ({
-        url: '/sanctum/csrf-cookie',
-        method: 'GET',
+        url: "/sanctum/csrf-cookie",
+        method: "GET",
       }),
     }),
     login: builder.mutation({
       query: (credentials) => ({
-        url: '/api/login',
-        method: 'POST',
+        url: "/api/login",
+        method: "POST",
         data: credentials,
       }),
     }),
     getAseguradoras: builder.query<Aseguradora[], void>({
       query: () => ({
-        method: 'GET',
+        method: "GET",
         url: `/api/aseguradoras`,
       }),
-      providesTags: ['aseguradoras'],
+      providesTags: ["aseguradoras"],
     }),
     getAseguradora: builder.query<Aseguradora, number>({
       query: (id) => ({
         url: `/api/aseguradoras/${id}`,
-        method: 'GET',
+        method: "GET",
       }),
-      providesTags: (_result, _error, id) => [{ type: 'aseguradora', id }]
+      providesTags: (_result, _error, id) => [{ type: "aseguradora", id }],
     }),
     crearAseguradora: builder.mutation<Aseguradora, Partial<Aseguradora>>({
       query: (aseguradora) => ({
         url: `/api/aseguradoras`,
-        method: 'POST',
+        method: "POST",
         data: aseguradora,
       }),
-      invalidatesTags: ['aseguradoras'],
+      invalidatesTags: ["aseguradoras"],
     }),
     modificarAseguradora: builder.mutation<Aseguradora, Partial<Aseguradora>>({
       query: (aseguradora) => ({
         url: `/api/aseguradoras/${aseguradora.id}`,
-        method: 'PUT',
+        method: "PUT",
         data: aseguradora,
       }),
-      invalidatesTags: (_result, _error, { id }) => [{ type: 'aseguradora', id}, 'aseguradoras'],
+      invalidatesTags: (_result, _error, { id }) => [
+        { type: "aseguradora", id },
+        "aseguradoras",
+      ],
     }),
     eliminarAseguradora: builder.mutation<void, number>({
       query: (id) => ({
         url: `/api/aseguradoras/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
-      invalidatesTags: (_result, _error, id) => [{ type: 'aseguradora', id }, 'aseguradoras'],
+      invalidatesTags: (_result, _error, id) => [
+        { type: "aseguradora", id },
+        "aseguradoras",
+      ],
     }),
     getClientes: builder.query<Cliente[], void>({
       query: () => ({
-        method: 'GET',
+        method: "GET",
         url: `/api/clientes`,
       }),
-      providesTags: ['clientes'],
+      providesTags: ["clientes"],
     }),
     getCliente: builder.query<Cliente, number>({
       query: (id) => ({
         url: `/api/clientes/${id}`,
-        method: 'GET',
+        method: "GET",
       }),
-      providesTags: (_result, _error, id) => [{ type: 'cliente', id }]
+      providesTags: (_result, _error, id) => [{ type: "cliente", id }],
     }),
     crearCliente: builder.mutation<Cliente, FormData>({
       query: (cliente) => ({
         url: `/api/clientes`,
-        method: 'POST',
+        method: "POST",
         data: cliente,
       }),
-      invalidatesTags: ['clientes'],
+      invalidatesTags: ["clientes"],
     }),
     modificarCliente: builder.mutation<Cliente, any>({
-      query: ({id,formData}) => ({
+      query: ({ id, formData }) => ({
         url: `/api/clientes/${id}`,
-        method: 'POST',
+        method: "POST",
         data: formData,
       }),
-      invalidatesTags: (_result, _error, { id }) => [{ type: 'cliente', id}, 'clientes'],
+      invalidatesTags: (_result, _error, { id }) => [
+        { type: "cliente", id },
+        "clientes",
+      ],
     }),
     eliminarCliente: builder.mutation<void, number>({
       query: (id) => ({
         url: `/api/clientes/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
-      invalidatesTags: (_result, _error, id) => [{ type: 'cliente', id }, 'clientes'],
+      invalidatesTags: (_result, _error, id) => [
+        { type: "cliente", id },
+        "clientes",
+      ],
     }),
     getPolizas: builder.query<any, void>({
       query: () => ({
-        method: 'GET',
+        method: "GET",
         url: `/api/polizas`,
       }),
-      providesTags: ['polizas'],
+      providesTags: ["polizas"],
     }),
     getPoliza: builder.query<any, number>({
       query: (id) => ({
         url: `/api/polizas/${id}`,
-        method: 'GET',
+        method: "GET",
       }),
       transformResponse: (response) => formatPoliza(response),
-      providesTags: (_result, _error, id) => [{ type: 'poliza', id }]
+      providesTags: (_result, _error, id) => [{ type: "poliza", id }],
     }),
     crearPoliza: builder.mutation<any, any>({
       query: (poliza) => ({
         url: `/api/polizas`,
-        method: 'POST',
+        method: "POST",
         data: poliza,
       }),
-      invalidatesTags: ['polizas'],
+      invalidatesTags: ["polizas"],
     }),
     modificarPoliza: builder.mutation<any, any>({
       query: (poliza) => ({
         url: `/api/polizas/${poliza.id}`,
-        method: 'PUT',
+        method: "PUT",
         data: poliza,
       }),
-      invalidatesTags: (_result, _error, { id }) => [{ type: 'poliza', id}, 'polizas'],
+      invalidatesTags: (_result, _error, { id }) => [
+        { type: "poliza", id },
+        "polizas",
+      ],
     }),
     eliminarPoliza: builder.mutation<void, number>({
       query: (id) => ({
         url: `/api/polizas/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
-      invalidatesTags: (_result, _error, id) => [{ type: 'poliza', id }, 'polizas'],
+      invalidatesTags: (_result, _error, id) => [
+        { type: "poliza", id },
+        "polizas",
+      ],
+    }),
+    getDashboard: builder.query<Dashboard, void>({
+      query: () => ({
+        method: "GET",
+        url: `/api/dashboard`,
+      }),
+      providesTags: ["dashboard"],
     }),
   }),
-})
+});
 
-export const { 
-  useLoginMutation, 
-  useLazyGetCSRFCookieQuery, 
-  useGetAseguradorasQuery, 
-  useGetAseguradoraQuery, 
-  useCrearAseguradoraMutation, 
-  useModificarAseguradoraMutation, 
+export const {
+  useLoginMutation,
+  useLazyGetCSRFCookieQuery,
+  useGetAseguradorasQuery,
+  useGetAseguradoraQuery,
+  useCrearAseguradoraMutation,
+  useModificarAseguradoraMutation,
   useEliminarAseguradoraMutation,
   useGetClientesQuery,
   useGetClienteQuery,
@@ -158,4 +192,5 @@ export const {
   useCrearPolizaMutation,
   useModificarPolizaMutation,
   useEliminarPolizaMutation,
-} = Api
+  useGetDashboardQuery,
+} = Api;
